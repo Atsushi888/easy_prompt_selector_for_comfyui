0b2fb5378f2bdf6f802cac21e4533094c2205ae6
Atsushi Nonaka <92051396+Atsushi888@users.noreply.github.com>
2026-01-06 16:31:50 +0900

    Add EasyPromptSelector mappings in __init__.py

--- easy_prompt_selector/__init__.py
+++ easy_prompt_selector/__init__.py
@@ -1,1 +1,9 @@
+from .selector import EasyPromptSelector
 
+NODE_CLASS_MAPPINGS = {
+    "EasyPromptSelector": EasyPromptSelector,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "EasyPromptSelector": "Easy Prompt Selector (Bangs)",
+}
